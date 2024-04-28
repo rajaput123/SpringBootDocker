66c15e1670bdf9bb62de491855e953f6cd48122b
Renuka <renukarajaput123@gmail.com>
2024-04-29 00:37:22 +0530

    first commit

---
+++ src/main/java/com/example/Asignment6Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asignment6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asignment6Application.class, args);
+	}
+
+}
